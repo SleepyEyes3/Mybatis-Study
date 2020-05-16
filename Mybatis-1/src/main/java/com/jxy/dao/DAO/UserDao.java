8e8380ada25feed84764ab19ef925dd6d143e635
Bruce_J <jxy941112@163.com>
2020-05-16 20:34:19 +0800

    Mybatis中的增删改查

--- Mybatis-1/src/main/java/com/jxy/dao/DAO/UserDao.java
+++ Mybatis-1/src/main/java/com/jxy/dao/DAO/UserDao.java
@@ -2,8 +2,21 @@ package com.jxy.dao.DAO;
 
 import com.jxy.dao.pojo.User;
 
+import java.net.UnknownServiceException;
 import java.util.List;
 
 public interface UserDao {
     List<User> getUserList();
+
+    //根据id查询用户
+    User getUserById(int id);
+
+    //添加用户
+    Integer insertUser(User user);
+
+    //更新用户
+    Integer updateUser(User user);
+
+    //删除用户
+    Integer deleteUserById(int id);
 }
--- Mybatis-1/src/test/java/Test1.java
+++ Mybatis-1/src/test/java/Test1.java
@@ -5,7 +5,12 @@ import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
 import java.util.List;
-
+/**
+ * 1. 增删改一定要加事物commit才能完成提交！
+ *
+ *
+ *
+ * */
 public class Test1 {
     @Test
     public void listTest(){
@@ -15,6 +20,49 @@ public class Test1 {
         for(User user:userList){
             System.out.println(user);
         }
+        sqlSession.close();
+    }
+
+    @Test
+    public void selectTest(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        UserDao mapper = sqlSession.getMapper(UserDao.class);
+        User userById = mapper.getUserById(1);
+        System.out.println(userById);
+        sqlSession.close();
+    }
+
+    @Test
+    public void insertTest(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserDao mapper = sqlSession.getMapper(UserDao.class);
+        Integer res = mapper.insertUser(new User(3, "jxy", "12345"));
+        if(res>0){
+            System.out.println("插入成功！");
+        }
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void updateTest(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserDao mapper = sqlSession.getMapper(UserDao.class);
+        Integer res = mapper.updateUser(new User(3, "jxy", "12345"));
+        if(res>0){
+            System.out.println("修改成功！");
+        }
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void deleteTest(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserDao mapper = sqlSession.getMapper(UserDao.class);
+        mapper.deleteUserById(3);
+        sqlSession.commit();
         sqlSession.close();
     }
 }
